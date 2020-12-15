961fcfa9f990180c0f7d84e0605e11098b03dae9
KRAK94 <andrekravchenko1994@mail.ru>
2020-12-15 22:19:56 +0300

    Updates

---
+++ precache-manifest.832baf573d10b817e081182280742f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ffc9b35f927c213997fb01cc216ca6",
+    "url": "/react-homeworks-master/index.html"
+  },
+  {
+    "revision": "57bf284776b9f35b70d5",
+    "url": "/react-homeworks-master/static/css/main.61d100bf.chunk.css"
+  },
+  {
+    "revision": "4c0a0fab57156bdab657",
+    "url": "/react-homeworks-master/static/js/2.8da126af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-homeworks-master/static/js/2.8da126af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57bf284776b9f35b70d5",
+    "url": "/react-homeworks-master/static/js/main.4cc5c228.chunk.js"
+  },
+  {
+    "revision": "be0615f83a3774d3d69f",
+    "url": "/react-homeworks-master/static/js/runtime-main.968b1d48.js"
+  }
+]);
