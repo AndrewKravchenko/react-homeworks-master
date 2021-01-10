2813a19139a492a3c1c13676bf0782706bfce9c1
KRAK94 <andrekravchenko1994@mail.ru>
2021-01-10 17:58:13 +0300

    Updates

---
+++ precache-manifest.f10d74217f425d64820072e766128086.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e993409dc7a61b265a0ba267efc98328",
+    "url": "/react-homeworks-master/index.html"
+  },
+  {
+    "revision": "2c17d2863f36af07829e",
+    "url": "/react-homeworks-master/static/css/main.74ebf79d.chunk.css"
+  },
+  {
+    "revision": "a8789044f624ef6f3945",
+    "url": "/react-homeworks-master/static/js/2.ee73e2bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-homeworks-master/static/js/2.ee73e2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c17d2863f36af07829e",
+    "url": "/react-homeworks-master/static/js/main.7b6e864b.chunk.js"
+  },
+  {
+    "revision": "be0615f83a3774d3d69f",
+    "url": "/react-homeworks-master/static/js/runtime-main.968b1d48.js"
+  },
+  {
+    "revision": "8dd80e30333ac5e01fc2c1c77534b4d0",
+    "url": "/react-homeworks-master/static/media/girl.8dd80e30.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-homeworks-master/precache-manifest.64466d445c0ec024a36f4e2b9d8e119f.js"
+  "/react-homeworks-master/precache-manifest.f10d74217f425d64820072e766128086.js"
 );
 
 self.addEventListener('message', (event) => {
